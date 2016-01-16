f6a45149b7cc61dceddb105c4f226324f20d8103
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2016-01-16 22:06:32 +0900

    fix test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,9 @@
+require "codeclimate-test-reporter"
+
+CodeClimate::TestReporter.configure do |config|
+  config.path_prefix = "lib"
+end
+
+CodeClimate::TestReporter.start
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'diff_resource'
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
